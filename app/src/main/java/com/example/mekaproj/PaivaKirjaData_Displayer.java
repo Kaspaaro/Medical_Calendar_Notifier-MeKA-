622f75a5e3d86f3854f4a3f016b80555ffead80e
Kaspar <kaspar.tullus@gmail.com>
2022-09-26 20:19:46 +0300

    Get all luodu,joka palauttaa uuden array listan. Uusi activity viewlistalla tehty.
    UPDATE

--- app/src/main/java/com/example/mekaproj/PaivaKirjaData_Displayer.java
+++ app/src/main/java/com/example/mekaproj/PaivaKirjaData_Displayer.java
@@ -3,14 +3,23 @@ package com.example.mekaproj;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 
 public class PaivaKirjaData_Displayer extends AppCompatActivity {
-
+    private ListView lv_Paivakirjadata;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paiva_kirja_data_displayer);
-
+        lv_Paivakirjadata = findViewById(R.id.pkDATADISPLAY);
+        MekaDataBase mekaDataBase = new MekaDataBase(PaivaKirjaData_Displayer.this);
+        List<PaivaKirjaData> everything = mekaDataBase.getEverything();
+        ArrayAdapter PaivakirjaArrayAdapter = new ArrayAdapter<PaivaKirjaData>(PaivaKirjaData_Displayer.this, android.R.layout.simple_list_item_1, everything);
+        lv_Paivakirjadata.setAdapter(PaivakirjaArrayAdapter);
     }
+
 }
