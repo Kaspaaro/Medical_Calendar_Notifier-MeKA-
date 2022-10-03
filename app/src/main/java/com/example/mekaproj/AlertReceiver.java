c49dbff6270f473f863a49fad54d7a7d8776153f
Kaspar <kaspar.tullus@gmail.com>
2022-10-03 22:05:34 +0300

    Notification layout changes

--- app/src/main/java/com/example/mekaproj/AlertReceiver.java
+++ app/src/main/java/com/example/mekaproj/AlertReceiver.java
@@ -35,8 +35,6 @@ public class AlertReceiver extends BroadcastReceiver {
         //here we set all the properties for the notification
         RemoteViews contentView = new RemoteViews(context.getPackageName(), R.layout.notification_layout);
         contentView.setImageViewResource(R.id.imageView4, R.drawable.logoremove);
-        PendingIntent pendingSwitchIntent = PendingIntent.getBroadcast(context, id, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        contentView.setOnClickPendingIntent(R.id.flashButton, pendingSwitchIntent);
         contentView.setTextViewText(R.id.message, text);
         contentView.setTextViewText(R.id.date, date);
         mBuilder.setSmallIcon(R.drawable.logoremove);
